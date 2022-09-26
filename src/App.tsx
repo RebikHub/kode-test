eb368d0285b84d3bc7de7566c7ba983c46e1b5be
RebikHub <rebikovlk@mail.ru>
2022-09-26 18:02:34 +0200

    Общая структура и компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import './App.css';
+import * as React from 'react';
+import { ReactElement } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Head from './components/Head';
+import ListPeople from './components/ListPeople';
+import Profile from './components/Profile';
 
-function App() {
+export default function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={
+        <>
+          <Head/>
+          <ListPeople/>
+        </>
+      }/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import { store } from './store/store';
 import App from './App';
+import Global from './styles/global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HashRouter>
+    <Provider store={store}>
+      <Global/>
+      <App/>
+    </Provider>
+  </HashRouter>
 );
 
